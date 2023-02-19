87a88763ca22951484bff92491b0bf06cb34b7a7
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-02-19 15:39:07 -0800

    updated index js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const manager = require('./lib/manager.js');
+const intern = require('./lib/intern.js');
+const engineer = require('./lib/engineer.js');
+const employee = require('./lib/employer.js');
