2bab5f5ab176f0060706895a9bae05f1846fa356
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-02-20 17:47:15 -0800

    added html body

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const manager = require('./lib/manager.js');
 const intern = require('./lib/intern.js');
 const engineer = require('./lib/engineer.js');
-const employee = require('./lib/employer.js');
+const employee = require('./lib/employee.js');
 
 //link to page creation
 const renderPage = require('./assets/page-render.js');
